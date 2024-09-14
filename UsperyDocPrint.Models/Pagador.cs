c4b15fdda61700c6fd1f5d391e46b94c161d97e7
Kaua Lima <kauavitorioof@gmail.com>
2024-09-14 13:20:12 -0300

    Clean up unused usings and reformat code for readability
    Removed unused `using` directives across multiple files to reduce unnecessary dependencies and improve code readability. Reformatted code in several files to use spaces instead of tabs for consistent indentation. Moved `using Newtonsoft.Json;` to the top in specific files to follow conventions. Made minor formatting changes in `ObjectGenerator.cs` for better readability. These changes enhance the maintainability and clarity of the codebase.

--- UsperyDocPrint.Models/Pagador.cs
+++ UsperyDocPrint.Models/Pagador.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UsperyDocPrint.Models
+﻿namespace UsperyDocPrint.Models
 {
     public class Pagador
     {
--- UsperyDocPrint/Controllers/ReceiptController.cs
+++ UsperyDocPrint/Controllers/ReceiptController.cs
@@ -1,9 +1,5 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Web;
 using System.Web.Mvc;
 using UsperyDocPrint.Models;
 
