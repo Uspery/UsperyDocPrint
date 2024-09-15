6da7ea8092b642db59d126b05626b86d7a9f3f85
Kaua Lima <kauavitorioof@gmail.com>
2024-09-15 12:32:55 -0300

    Update HelpPage route name to HelpPage_Default_v2
    Changed the route name in the RegisterArea method of the HelpPageAreaRegistration class from "HelpPage_Default" to "HelpPage_Default_v2". This update likely reflects a versioning change in the Help Page routing configuration, indicating a version 2 of the Help Page route.

--- UsperyDocPrint/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ UsperyDocPrint/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -16,7 +16,7 @@ namespace UsperyDocPrint.Areas.HelpPage
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
-                "HelpPage_Default",
+                "HelpPage_Default_v2",
                 "Help/{action}/{apiId}",
                 new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
 
